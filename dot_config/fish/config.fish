26de339f0f2756984d5bbfc4fb44336d1c96e218
jzcurious <paranoid.hpc.io@gmail.com>
2026-08-03 11:30:30 +0700

    add configs

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,13 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# быстрые команды для управления zapret
+alias zapret-config='$HOME/zapret-configs/install.sh'
+alias zapret-utils='$HOME/zapret-configs/utils-zapret.sh'
+alias z='zoxide'
+alias zed='zeditor'
+alias l='lsd -l'
+alias icat='kitten icat'
+
+function fish_greeting
+    # do nothing
+end
